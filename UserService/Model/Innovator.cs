a3420d16059b41840bed4add82e303ba9626e308
aakashk.ujjain <aakashk.ujjain@gmail.com>
2023-09-12 16:17:41 +0000

    add foldder

---
+++ UserService/Model/Innovator.cs
@@ -0,0 +1,15 @@
+﻿namespace UserService.Model
+{
+    public class Innovator
+    {
+        public string InnovatorID { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
