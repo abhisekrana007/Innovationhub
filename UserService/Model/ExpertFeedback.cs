dd9a2480b87d0799dc6a9312228d52ff8588dccb
aakashk.ujjain <aakashk.ujjain@gmail.com>
2023-09-14 18:07:37 +0000

    Added expertfeedback

---
+++ UserService/Model/ExpertFeedback.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UserService.Model
+{
+   public class ExpertFeedback
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string FeedbackID { get; set; }
+
+        public string ExpertID { get; set; }
+
+        public string InnovatorID { get; set; }
+
+        public string FeedbackContent { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ UserService/Service/IExpertFeedbackService.cs
@@ -0,0 +1,17 @@
+﻿using UserService.Model;
+
+namespace UserService.Service
+{
+    public interface IExpertFeedbackService
+    {
+        Task<IEnumerable<ExpertFeedback>> GetAllFeedbackAsync();
+
+        Task<ExpertFeedback> GetFeedbackByIdAsync(string id);
+
+        Task CreateFeedbackAsync(ExpertFeedback feedback);
+
+        Task UpdateFeedbackAsync(string id, ExpertFeedback feedback);
+
+        Task DeleteFeedbackAsync(string id);
+    }
+}
