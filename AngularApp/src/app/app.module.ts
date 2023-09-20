dab1542965d02f6f8cda7029bae4a639344f359a
abhisekrana007 <abhisekrana007@gmail.com>
2023-09-20 07:31:23 +0000

    Added Basic Structure Frontend

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, components } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -20,6 +20,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card'; 
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ErrorCatchingInterceptor } from './interceptor/error-catching.interceptor';
@@ -28,13 +29,14 @@ import { ErrorCatchingInterceptor } from './interceptor/error-catching.intercept
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    components
   ],
   imports: [
     BrowserModule,BrowserAnimationsModule,MatExpansionModule,
     MatToolbarModule,FormsModule,HttpClientModule,MatSlideToggleModule,
     MatButtonModule,MatIconModule,MatDividerModule,MatCardModule,MatFormFieldModule,
-    ReactiveFormsModule,AppRoutingModule
+    ReactiveFormsModule,AppRoutingModule,MatTabsModule
 
   ],
   providers: [LoginService],
