fa1c7fd58a03be17a2d12cc0d102cda2b011c769
rupalisinha2001 <rupalisinha2001@gmail.com>
2023-09-20 08:08:11 +0000

    added

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, components } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -24,7 +24,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ErrorCatchingInterceptor } from './interceptor/error-catching.interceptor';
-import { RegistrationComponent } from './components/registration/registration.component';
+
 import { RegistrationService } from './services/registration.service';
 
 
@@ -32,7 +32,9 @@ import { RegistrationService } from './services/registration.service';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistrationComponent
+
+   
+
   ],
   imports: [
     BrowserModule,BrowserAnimationsModule,MatExpansionModule,
